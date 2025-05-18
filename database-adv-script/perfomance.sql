3b9aa62069f0eab0a3d5c31d66063d38dfc893e6
Med-belhaj <simohamed.belhaj2@gmail.com>
2025-05-18 22:31:27 +0100

    Update perfomance.sql

--- database-adv-script/perfomance.sql
+++ database-adv-script/perfomance.sql
@@ -1,6 +1,7 @@
 -- Task 4. Initial Complex Query for Performance Testing
 
--- Retrieve bookings with user, property, and payment details
+-- 4.1 Measure performance of initial query using EXPLAIN ANALYZE
+EXPLAIN ANALYZE
 SELECT
   b.id               AS booking_id,
   u.first_name       AS user_first,
@@ -14,4 +15,6 @@ JOIN users AS u
 JOIN properties AS p
   ON b.property_id = p.id
 LEFT JOIN payments AS pay
-  ON b.id = pay.booking_id;
+  ON b.id = pay.booking_id
+WHERE b.check_in >= '2025-01-01'
+  AND b.check_out <= '2025-12-31';
